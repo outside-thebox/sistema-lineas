1cfb742e6756e352eaf734b638cfc184cdea3b9b
Damián Ladiani <damian.ladiani@minseg.gob.ar>
2017-02-01 13:24:13 +0000

    Progreso con el formulario de cuentas

--- resources/views/cuentas/formulario.blade.php
+++ resources/views/cuentas/formulario.blade.php
@@ -3,16 +3,42 @@
 	
 	<h1>@{{ name }}</h1>
 
-@endsection
+	{!! Form::model(isset($cuenta) ? $cuenta:null ,[], ['role' => 'form']) !!}
+		
+		<div class="col-md-6">
+			{{ Form::label('nro_cuenta','Nro de cuenta') }}
+			{{ Form::text('nro_cuenta',null,['v-model' => 'nro_cuenta','placeholder' => 'Introduce el nro de cuenta', 'class' => 'form-control']) }}
+		</div>
+		<div class="col-md-6">
+			{{ Form::label('nombre_cuenta','Nombre de la cuenta') }}
+			{{ Form::text('nombre_cuenta',null,['v-model' => 'nombre_cuenta','placeholder' => 'Introduce el nombre de la cuenta', 'class' => 'form-control']) }}
+		</div>
+		<div class="col-md-6">
+			{{ Form::label('dominio','Dominio') }}
+			{{ Form::text('dominio',null,['v-model' => 'dominio','placeholder' => 'Introduce el dominio', 'class' => 'form-control']) }}
+		</div>
+		<div class="col-md-6">
+			{{ Form::label('server_principal','Nombre del server principal') }}
+			{{ Form::text('server_principal',null,['v-model' => 'server_principal','placeholder' => 'Introduce nombre del server principal', 'class' => 'form-control']) }}
+		</div>
+		<div class="col-md-6">
+			{{ Form::label('server_backup','Nombre del server backup') }}
+			{{ Form::text('server_backup',null,['v-model' => 'server_backup','placeholder' => 'Introduce nombre del server backup', 'class' => 'form-control']) }}
+		</div>
 
+		<div class="col-md-12">
+	        <a href="{!! route('cuentas.index') !!}" class="btn btn-success pull-right" style="margin-right: 10px">Cancelar</a>
+			{!! Form::button("Guardar", ['type' => 'submit', 'class' => 'btn btn-primary pull-right']) !!}
+		</div>
+	{!! Form::close() !!}
+@endsection
 @section('scripts')
 	<script>
 		new Vue({
 			el: '#main',
-			data: {
-				name: 'Boedo'			
+			data:{
+				name: 'Agregar una cuenta'			
 			}
 		});
 	</script>
 @endsection
-
--- resources/views/cuentas/index.blade.php
+++ resources/views/cuentas/index.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 	<h1>Listado de cuentas</h1>
 
-	<a href="{{ route('cuentas.create') }}">Agregar</a>
+	<a href="{{ route('cuentas.create') }}" class="btn btn-primary">Agregar</a>
 @endsection
 
